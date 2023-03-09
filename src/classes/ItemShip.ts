fbcd09c6c8a8826ff40e4452e07a990688355a51
Pierre29G <pierre29g@gmail.com>
2023-03-09 16:12:49 +0100

    Cleaned ItemPlayer & ItemShip

--- src/classes/ItemShip.ts
+++ src/classes/ItemShip.ts
@@ -1,38 +1,14 @@
 import Item from "./Item";
 
 class ItemShip extends Item {
-    private _type: string;
-    private _category: string;
     private _stat: [{name: string, modifier: number}];
   
-    constructor(title: string, description: string, stat: [{name: string, modifier: number}], type: string, category: string) {
+    constructor(id: string, title: string, description: string, stat: [{name: string, modifier: number}], type: string, category: string) {
 
-        super(title, description);
-
-        // Either Object or Effect
-        this._type = type;
-
-        // Can be shoes or helmet or weapon or whatever
-        this._category = category;
+        super(id, title, description, type, category);
 
         // {name: 'strenght',modifier: '2'}
         this._stat = stat;
-      }
-
-      public get type(){
-        return this._type;
-      }
-    
-      public set type(type: string){
-        this._type = type;
-      }
-
-      public get category(){
-        return this._category;
-      }
-    
-      public set category(category: string){
-        this._category = category;
       }
 
       public get stat(){
