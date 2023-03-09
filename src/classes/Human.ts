b402279411b16c8b7b0566511e07b457e5e38d00
Pierre29G <pierre29g@gmail.com>
2023-03-09 16:00:43 +0100

    Race subclass Human

---
+++ src/classes/Human.ts
@@ -0,0 +1,12 @@
+import Race from "./Race";
+
+class Human extends Race {
+
+  constructor(name = 'Humain', maxHealth = 100, strenght = 5, intelligence = 5, luck = 5) {
+    
+    super(name, maxHealth, strenght, intelligence, luck);
+  }
+
+}
+
+export default Human;
