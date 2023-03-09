0f5691fc4d5406ccd698b9ed69966f46218e7171
Pierre29G <pierre29g@gmail.com>
2023-03-09 14:13:53 +0100

    Class itemPlayer

---
+++ src/classes/ItemPlayer.ts
@@ -0,0 +1,48 @@
+import Item from "./Item";
+
+class ItemPlayer extends Item {
+    private _type: string;
+    private _category: string;
+    private _stat: [{name: string, modifier: number}];
+  
+    constructor(title: string, description: string, stat: [{name: string, modifier: number}], type: string, category: string) {
+
+        super(title, description);
+
+        // Either Object or Effect
+        this._type = type;
+
+        // Can be shoes or helmet or weapon or whatever
+        this._category = category;
+
+        // {name: 'strenght',modifier: '2'}
+        this._stat = stat;
+      }
+
+      public get type(){
+        return this._type;
+      }
+    
+      public set type(type: string){
+        this._type = type;
+      }
+
+      public get category(){
+        return this._category;
+      }
+    
+      public set category(category: string){
+        this._category = category;
+      }
+
+      public get stat(){
+        return this._stat;
+      }
+    
+      public set stat(stat: [{name: string, modifier: number}]){
+        this._stat = stat;
+      }
+
+}
+
+export default ItemPlayer;
