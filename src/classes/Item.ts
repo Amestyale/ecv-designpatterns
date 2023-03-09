b7b8a32220202ec9c00486a7876482702e018628
Pierre29G <pierre29g@gmail.com>
2023-03-09 16:12:31 +0100

    Added id type & category to class item

--- src/classes/Item.ts
+++ src/classes/Item.ts
@@ -1,19 +1,28 @@
 class Item {
-  private _id: number;
+  private _id: string;
   private _title: string;
   private _description: string;
+  
+  private _type: string;
+  private _category: string;
 
-  constructor(id: number, title: string, description: string) {
+  constructor(id: string, title: string, description: string, type: string, category: string) {
     this._id = id;
     this._title = title;
     this._description = description;
+
+    // Either Object or Effect
+    this._type = type;
+
+    // Can be shoes or helmet or weapon or whatever
+    this._category = category;
   }
 
   public get id(){
     return this._id;
   }
 
-  public set id(id: number){
+  public set id(id: string){
     this._id = id;
   }
 
@@ -31,6 +40,22 @@ class Item {
 
   public set description(description: string){
     this._description = description;
+  }
+
+  public get type(){
+    return this._type;
+  }
+
+  public set type(type: string){
+    this._type = type;
+  }
+
+  public get category(){
+    return this._category;
+  }
+
+  public set category(category: string){
+    this._category = category;
   }
 }
 
