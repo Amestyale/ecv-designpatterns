6d31d0a7e549f428051ca8772817fe6d6d68a687
Pierre29G <pierre29g@gmail.com>
2023-03-09 14:15:20 +0100

    Stat supression from player

--- src/classes/Player.ts
+++ src/classes/Player.ts
@@ -5,19 +5,13 @@ class Player {
   private _health: number;
   private _money: number;
   private _race: Race;
-  private _strenght: number;
-  private _intelligence: number;
-  private _luck: number;
 
   private _inventory: [ItemPlayer];
 
-  constructor(health: number, money: number, race: Race, strenght: number, intelligence: number, luck: number, inventory: [ItemPlayer]) {
+  constructor(health: number, money: number, race: Race, inventory: [ItemPlayer]) {
     this._health = health;
     this._money = money;
     this._race = race;
-    this._strenght = strenght;
-    this._intelligence = intelligence;
-    this._luck = luck;
 
     //TODO Define how inventory work
     this._inventory = inventory;
@@ -45,30 +39,6 @@ class Player {
 
   public set race(race: Race){
     this._race = race;
-  }
-
-  public get strenght(){
-    return this._strenght;
-  }
-
-  public set strenght(strenght: number){
-    this._strenght = strenght;
-  }
-
-  public get intelligence(){
-    return this._intelligence;
-  }
-
-  public set intelligence(intelligence: number){
-    this._intelligence = intelligence;
-  }
-
-  public get luck(){
-    return this._luck;
-  }
-
-  public set luck(luck: number){
-    this._luck = luck;
   }
 
   public get inventory(){
