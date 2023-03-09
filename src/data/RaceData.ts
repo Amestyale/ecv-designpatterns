c1ec154d9f6366b1ddc716af9146914f45450051
Pierre29G <pierre29g@gmail.com>
2023-03-09 15:33:30 +0100

    Data for races

---
+++ src/data/RaceData.ts
@@ -0,0 +1,40 @@
+let RaceData = 
+[
+    {
+        name: 'Humain',
+        maxHealth: '100',
+        strenght: '5',
+        intelligence: '5',
+        luck: '5'
+    },
+    {
+        name: 'Petit Homme Vert',
+        maxHealth: '90',
+        strenght: '4',
+        intelligence: '7',
+        luck: '6'
+    },
+    {
+        name: 'Loutre géante',
+        maxHealth: '130',
+        strenght: '7',
+        intelligence: '3',
+        luck: '5'
+    },
+    {
+        name: 'Laïka, la petite chienne russe',
+        maxHealth: '80',
+        strenght: '4',
+        intelligence: '6',
+        luck: '1'
+    },
+    {
+        name: 'Projection Astral',
+        maxHealth: '50',
+        strenght: '1',
+        intelligence: '10',
+        luck: '10'
+    },
+];
+
+export default RaceData;
