1b11e33cc38a5e247660211d4d343f7961a337b0
Johan Deleon <johdel390@gmail.com>
2023-03-10 16:40:37 +0100

    🐛 Planets position

--- src/data/PlanetData.ts
+++ src/data/PlanetData.ts
@@ -20,7 +20,7 @@ let PlanetData =
     },
     {
         id: 3,
-        x: 750,
+        x: 500,
         y: 0,
         name: 'Planète commerciale ID-84B',
         description: 'Dès votre arrivée au abord de cette étrange boule couverte de signes fluorescent, vous êtes absolument bombardé de publicité pour divers magasins et services.<br>*BIENVENUE SUR ID-84B VOTRE RÉFÉRENCE EN SHOPPING INTERSTELLAIRE*<br>Heureusement, le parking à vaisseau n’a pas l’air payant…',
@@ -29,7 +29,7 @@ let PlanetData =
     },
     {
         id: 4,
-        x: 600,
+        x: 750,
         y: 0,
         name: 'Royaume des clowns',
         description: 'Mon Dieu quel spectacle !<br>Des millions de clowns peuvent être vus à la surface de la planète, performant des activités classiques de clown tel que le lancer de tarte à la crème ou la course de monocycle.<br>Une cacophonie effroyable de klaxon et de bruit de chaussure trop grande se fait entendre dès la sortie du vaisseau. L’escale semble déjà très longue et pourtant vous n’êtes là que depuis cinq minutes.',
