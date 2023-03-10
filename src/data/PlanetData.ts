f71703872b33bf5e697d28605c8f64ac009e7e20
Pierre29G <49999698+Pierre29G@users.noreply.github.com>
2023-03-10 11:06:46 +0100

    Content (#11)
    * Data for player objects
    
    * Data for races
    
    * Race subclass Human
    
    * Added comment in RaceData
    
    * Added id on class item
    
    * Added id type & category to class item
    
    * Cleaned ItemPlayer & ItemShip
    
    * Added new item
    
    * Planet Data
    
    * Room Data
    
    ---------
    
    Co-authored-by: Clément Verger <80526212+clemeverger@users.noreply.github.com>

---
+++ src/data/PlanetData.ts
@@ -0,0 +1,19 @@
+let PlanetData = 
+[
+    {
+        id: 1,
+        name: 'Planète gelée',
+        description: 'La première chose que vous remarquez en atterrissant est la température absolument glaciale qui règne sur ce globe froid et sans vie.<br>Une banquise géante est la seule chose visible à perte de vue, le vent fait virevolter la fine couche de neige présente sur le sol…',
+        appearance: 'blanche',
+        rooms: [10,11],
+    },
+    {
+        id: 2,
+        name: 'La Terre ?',
+        description: 'En arrivant en orbite, vous avez déjà pu reconnaitre plusieurs continents comme l’Afrique et l’Eurasie.<br>Mais maintenant que vous avez atterri, vous en êtes sûr, vous êtes sur Terre !<br>Sauf que cette dernière se situe à plusieurs millions d’années-lumière d’ici.<br>Restez sur vos gardes…',
+        appearance: 'familière',
+        rooms: [20,21],
+    },
+];
+
+export default PlanetData;
