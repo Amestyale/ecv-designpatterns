b2aa2760fbc14cedf212b7f4043553a8c8e3b677
Johan Deleon <johdel390@gmail.com>
2023-04-07 12:24:40 +0200

    🔥Simplify Vehicle

--- src/classes/Vehicle.ts
+++ src/classes/Vehicle.ts
@@ -1,42 +1,16 @@
 class Vehicle {
-    private _name: string;
-    private _maxFuel: number;
-    private _maxHealth: number;
-    private _maxShield: number;
+    public name: string;
+    public maxFuel: number;
+    public maxHealth: number;
+    public maxShield: number;
 
     constructor(name: string, maxFuel: number, maxHealth: number, maxShield: number) {
-        this._name = name;
-        this._maxFuel = maxFuel; 
-        this._maxHealth = maxHealth;  
-        this._maxShield = maxShield;     
+        this.name = name;
+        this.maxFuel = maxFuel; 
+        this.maxHealth = maxHealth;  
+        this.maxShield = maxShield;     
     }
 
-    public get name() {
-        return this._name;
-    }
-    public set name(name: string) {
-        this._name = name;
-    }
-
-    public get maxFuel() {
-        return this._maxFuel;
-    }
-    public set maxFuel(maxFuel: number) {
-        this._maxFuel = maxFuel;
-    }
-
-    public get maxHealth() {
-        return this._maxHealth;
-    }
-    public set maxHealth(maxHealth: number) {
-        this._maxHealth = maxHealth;
-    }
-
-    public get maxShield() {
-        return this._maxShield;
-    }
-    public set maxShield(maxShield: number) {
-        this._maxShield = maxShield;
-    }
 }
+
 export default Vehicle;
