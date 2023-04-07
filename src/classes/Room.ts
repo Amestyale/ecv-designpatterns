6ab6e5ed758adaa8649db7162a44eed84eada40c
Valentin Afonso <valentinafonso22@gmail.com>
2023-04-07 12:24:12 +0200

    clean room class

--- src/classes/Room.ts
+++ src/classes/Room.ts
@@ -1,33 +1,18 @@
 import Option from "./gamemanagement/Option"
 
 export default class Room {
-  private _id: number
-  private _title: string
-  private _text: string
-  private _options: Option[]
-  private _optionFacade: string
+  public id: number
+  public title: string
+  public text: string
+  public options: Option[]
+  public optionFacade: string
 
   constructor(id: number, title: string, text: string, options: Option[], optionFacade: string) {
-    this._id = id
-    this._title = title
-    this._text = text
-    this._options = options
-    this._optionFacade = optionFacade
+    this.id = id
+    this.title = title
+    this.text = text
+    this.options = options
+    this.optionFacade = optionFacade
   }
 
-  get id(): number {
-    return this._id
-  }
-  get title(): string {
-    return this._title
-  }
-  get text(): string {
-    return this._text
-  }
-  get options(): Option[] {
-    return this._options
-  }
-  get optionFacade(): String {
-    return this._optionFacade
-  }
 }
