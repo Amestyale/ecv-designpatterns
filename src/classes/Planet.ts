1cd2d5c9b6dad46be01b279621b1a140dbc82ce4
Valentin Afonso <valentinafonso22@gmail.com>
2023-04-07 12:21:36 +0200

    clean race class

--- src/classes/Planet.ts
+++ src/classes/Planet.ts
@@ -1,44 +1,20 @@
 import Room from './Room'
 
 export default class Planet {
-  private _name: string
-  private _description: string
-  private _appearance: string
-  private _rooms: Room[]
-  private _x: number
-  private _y: number
+  public name: string
+  public description: string
+  public appearance: string
+  public rooms: Room[]
+  public x: number
+  public y: number
 
   constructor(x: number, y: number, name: string, description: string, appearance: string, rooms: Room[]) {
-    this._x = x
-    this._y = y
-    this._name = name
-    this._description = description
-    this._appearance = appearance
-    this._rooms = rooms
-  }
-
-  get x(): number {
-    return this._x
-  }
-
-  get y(): number {
-    return this._y
-  }
-
-  get name(): string {
-    return this._name
-  }
-
-  get description(): string {
-    return this._description
-  }
-
-  get appearance(): string {
-    return this._appearance
-  }
-
-  get rooms(): Room[] {
-    return this._rooms
+    this.x = x
+    this.y = y
+    this.name = name
+    this.description = description
+    this.appearance = appearance
+    this.rooms = rooms
   }
 
   public distanceFrom(x: number, y: number): number {
