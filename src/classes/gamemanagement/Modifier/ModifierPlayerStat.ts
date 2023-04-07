3dd4a141a24260cbb691c35ca2fee8a42516ca66
Johan Deleon <johdel390@gmail.com>
2023-04-07 12:21:47 +0200

    🔥Simplify Modifiers

--- src/classes/gamemanagement/Modifier/ModifierPlayerStat.ts
+++ src/classes/gamemanagement/Modifier/ModifierPlayerStat.ts
@@ -4,39 +4,31 @@ import Player from "../../Player";
 import Modifier from "./Modifier";
 
 export default class ModifierPlayerStat extends Modifier {
-  private _stat: PlayerDataList 
-  private _player: Player
+  public stat: PlayerDataList 
+  public player: Player
 
   constructor(player: Player, stat: PlayerDataList, value: number){
     super(value)
-    this._stat = stat
-    this._player = player
-  }
-
-  public get stat(): any {
-    return this._stat;
-  }
-
-  public set stat(stat: any){
     this.stat = stat
+    this.player = player
   }
 
   public canBeChoosen(){
-    switch(this._stat){
+    switch(this.stat){
       case PlayerDataList.health:
-        return (this._player.health + +this.value) > 0
+        return (this.player.health + +this.value) > 0
       case PlayerDataList.money:
-        return (this._player.money + +this.value) > 0
+        return (this.player.money + +this.value) > 0
     }
   }
 
   public apply(){
     switch(this.stat){
       case PlayerDataList.health:
-        this._player.health = this._player.health + +this.value
+        this.player.health = this.player.health + +this.value
         break;
       case PlayerDataList.money:
-        this._player.money = this._player.money + +this._value
+        this.player.money = this.player.money + +this.value
         break;
     }
   }
