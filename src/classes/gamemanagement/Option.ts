224177b7f29547b5ebbeefea73c7dab586e0853c
Johan Deleon <johdel390@gmail.com>
2023-04-07 12:22:51 +0200

    🔥Simplify Option

--- src/classes/gamemanagement/Option.ts
+++ src/classes/gamemanagement/Option.ts
@@ -1,34 +1,14 @@
 import Modifier from "./Modifier/Modifier";
 
 export default class Option{
-  private _title: String;
-  private _text: String;
-  private _modifiers: Array<Modifier>
+  private title: String;
+  private text: String;
+  private modifiers: Array<Modifier>
 
   constructor(title: String, text: String, modifiers: Array<Modifier>){
-    this._title = title
-    this._text = text
-    this._modifiers = modifiers
-  }
-
-  public get title(): String{
-    return this._title
-  }
-
-  public set title(title: String){
-    this._title = title
-  }
-
-  public get text(): String{
-    return this._text
-  }
-  
-  public set text(text: String){
-    this._text = text
-  }
-
-  public get modifiers(): Array<Modifier>{
-    return this._modifiers
+    this.title = title
+    this.text = text
+    this.modifiers = modifiers
   }
 
   public canBeChoosen(){
