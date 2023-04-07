8538954f621391521463c3c8ab9565bedadb86b2
Johan Deleon <johdel390@gmail.com>
2023-04-07 12:27:50 +0200

    🔥Simplify Player

--- src/classes/Player.ts
+++ src/classes/Player.ts
@@ -4,30 +4,17 @@ import Ship from './Ship'
 class Player {
   public health: number
   public money: number
-  private _race: Race
+  public race: Race
 
-  private _ship: Ship
+  public ship: Ship
 
   constructor(race: Race, money: number, ship: Ship) {
-    this._race = race
+    this.race = race
     this.money = money
     this.health = race.maxHealth
-    //TODO Define how inventory work
-
-    this._ship = ship
+    this.ship = ship
   }
-
-  public get race() {
-    return this._race
-  }
-
-  public set race(race: Race) {
-    this._race = race
-  }
-
-  public get ship() {
-    return this._ship
-  }
+  
 
 }
 
