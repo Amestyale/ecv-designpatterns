63436a868640635cde794971617bbf4dce238128
Pierre29G <pierre29g@gmail.com>
2023-04-14 16:09:11 +0200

    Called log component in Game.tsx

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Heading } from '@chakra-ui/layout'
+import { Heading,UnorderedList,ListItem } from '@chakra-ui/layout'
 import GameController from '../classes/GameController'
 import GameDisplay from '../classes/gamemanagement/GameDisplay'
 import Option from '../classes/gamemanagement/Option'
@@ -7,6 +7,7 @@ import FlightInstruments from './FlightInstruments'
 import FlightInstrument from './FlightInstrument'
 import { FaCoins, FaGasPump, FaHeart } from 'react-icons/fa'
 import Story from './Story'
+import Log from './Log'
 import FlightInstrumentGauge from './FlightInstrumentGauge'
 
 type GameProps = {
@@ -19,6 +20,7 @@ const Game = ({ gameController }: GameProps) => {
   const text = gameController.currentRoom()?.text
   const options = gameController.currentRoom()?.options
   const optionFacade = gameController.currentRoom()?.optionFacade
+  const logText = gameController.log
 
   const [refresh, setRefresh] = useState(false)
   const gameDisplay = new GameDisplay(gameController)
@@ -33,6 +35,9 @@ const Game = ({ gameController }: GameProps) => {
   return (
     <>
       <FlightInstruments instruments={[<FlightInstrument text={distanceFromWin}></FlightInstrument>]} />
+      <Log
+        text={logText}
+      />
       <Story
         title={title}
         text={text}
