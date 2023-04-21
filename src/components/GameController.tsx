dd2347db7184a042ae2c18ebe4ecc36d961a3ba0
Johan Deleon <johdel390@gmail.com>
2023-04-21 13:01:13 +0200

    💚 Fix build errors

--- src/components/GameController.tsx
+++ src/components/GameController.tsx
@@ -1,8 +1,6 @@
 import { Heading } from '@chakra-ui/layout'
 import Player from '../classes/Player'
 import useGameController from '../hooks/useGameController'
-import usePlanets from '../hooks/usePlanets'
-import useRooms from '../hooks/useRooms'
 import Game from './Game'
 
 type GameControllerProps = {
