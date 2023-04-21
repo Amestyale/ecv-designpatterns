8e47b2ea8a6fb8705257d3face2c6723f31165d9
Johan Deleon <johdel390@gmail.com>
2023-04-21 11:38:28 +0200

    ✨ Backup types

---
+++ src/classes/gamemanagement/BackupTypes.ts
@@ -0,0 +1,41 @@
+
+
+export type BackupItem = {
+  id: string
+  name: string
+  description: string
+  image: string
+}
+
+export type BackupShip = {
+  name: string
+
+  fuel: number
+  maxfuel: number
+
+  health: number
+  maxHealth: number
+}
+
+export type BackupPlayer = {
+  luck: number,
+  money: number,
+  health: number,
+
+  ship: BackupShip
+  items: Array<BackupItem>
+}
+
+export type BackupGame = {
+  player: BackupPlayer
+  planets: Array<number>
+
+  currentRoomIndex: number
+
+  inspace: boolean
+  inevent: boolean
+  canevent: boolean
+
+  log: String
+  distance: number
+}
