14fa6f646713204861648b6d811c0808e22d3cab
Pierre29G <pierre29g@gmail.com>
2023-04-21 15:40:22 +0200

    Added ItemsData

---
+++ src/data/ItemsData.ts
@@ -0,0 +1,45 @@
+let ItemsData = 
+[
+    {
+        id: 'pistolet_laser',
+        title: 'Pistolet Laser',
+        description: 'Cette arme étrange ressemble fortement à un jouet en plastique, mais elle tire des rayons mortels.',
+    },
+    {
+        id: 'cerveau_parasite_flottant',
+        title: 'Cerveau parasite flottant',
+        description: 'Créature fascinante et étrange, ce cerveau télépathe se nourrit de la force vitale de son hôte en échange de connaissances.',
+    },
+    {
+        id: 'champ_de_force_personnel_tx_4670',
+        title: 'Champ de force personnel TX-4670',
+        description: 'Merveille de technologie crombonienne, le TX-4670 vous protégera des dangers de type organique tels que les piranhas radioactifs ou les belles-mères.',
+    },
+    {
+        id: 'contusion_legere',
+        title: 'Contusion légère',
+        description: 'Bien que douloureuse sur le moment, cette blessure ne devrait pas laisser de séquelle à part une sacrée bosse et une vision un peu flou.',
+    },
+    {
+        id: 'dechirure_dans_l_espace_temps',
+        title: 'Déchirure dans l’espace temps',
+        description: 'C’est marrant de savoir que le nom scientifique de ce genre de chose est transition de flop. C’est moins marrant d’en subir une…',
+    },
+    {
+        id: 'grande_malediction_sacree',
+        title: 'Grande malédiction sacrée de l’empereur intergalactique de la souffrance',
+        description: 'Avec un nom comme cela, on s’attendait à bien pire…',
+    },
+    {
+        id: 'presence_douteuse',
+        title: 'Présence douteuse',
+        description: 'La chose qui errait dans le monolithe vous a suivi, ses intentions à votre égard restent trouble.',
+    },
+    {
+        id: 'biscuits_durs',
+        title: 'Boîte de biscuits très, très durs',
+        description: 'Croquer dans l\'un de ces biscuits risque bien de vous casser une dent !',
+    },
+];
+
+export default ItemsData;
